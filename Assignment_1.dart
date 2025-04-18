cfcd296c8e64c50de3722f2ce2dc84d365747471
hassamshaikh <hassamshaikh94@gmail.com>
2025-04-18 11:10:37 +0500

    done assignment 1 in dart

---
+++ Assignment_1.dart
@@ -0,0 +1,10 @@
+void main() {
+ String stdName = "Hassam"; int stdage = 27;
+ String stdClass = "Masters"; double stdPercent = 82.7; double stdPrevRslt = 85.3;
+ print("---Introduction---");
+ print("My name is ${stdName}");
+ print("My age is ${stdAge}");
+ print("My qualification is ${stdClass}");
+ print("I got ${stdPercent}% marks in masters");
+ print("I got ${stdPrevRslt}% marks in graduation");
+}
